b8fed2dcd62d9b6a41ec2abdfdcc79abd84165ed
Sergey Kulikov <kulikov112@users.noreply.github.com>
2017-09-05 12:00:45 +0300

    fix pulse tags export (#17)
    MHD-128

--- lib/ooyala_player/pulse_tag_field.rb
+++ lib/ooyala_player/pulse_tag_field.rb
@@ -4,16 +4,23 @@ module OoyalaPlayer
     register_instance_option :formatted_value do
       o = bindings[:object]
       v = bindings[:view]
-      I18n.available_locales.map do |locale|
+      tags = I18n.available_locales.map do |locale|
         o.class.video_columns.map do |column|
           video = o.try(column == :ooyala_video_id ? "video" : "video_for_#{column}", locale)
           if video.present?
             tags = video.tags
             tags = 'no_tags' if tags.blank?
-            "Tags for #{column}[#{locale}] #{v.link_to tags, v.show_path(model_name: 'OoyalaPlayer~Video', id: video.id)}"
+            "Tags for #{column}[#{locale}] " +
+            if v.present?
+                "#{v.link_to tags, v.show_path(model_name: 'OoyalaPlayer~Video', id: video.id)}"
+            else
+              tags
+            end
           end
         end.compact.join('<br/>')
       end.reject(&:empty?).join('<br/>').html_safe()
+      tags.gsub! '<br/>', "\n" if v.nil?
+      tags
     end
 
     register_instance_option :partial do
